bfec89f5d048bf152dad442ed9c14319ac14a274
Carlos Bravo-Prieto <charl_ap@hotmail.es>
2026-07-28 10:57:36 +0200

    Update main_code.py

--- Regression/main_code.py
+++ Regression/main_code.py
@@ -282,7 +282,7 @@ class QuantumModel(nn.Module):
         self.c = c  # nn.Parameter(torch.tensor(c, dtype=torch.float32))
 
     def forward(self, x):
-        return self.q_layer(x)*self.c  #torch.exp(self.c) # for trainable c I found exp(c) working better
+        return self.q_layer(x)*self.c  #torch.exp(self.c) # for trainable c
         
 def train_model(model, X_train, y_train, X_val, y_val, X_test, y_test,
                 epochs=3500, lr=1, loss=nn.MSELoss(),
@@ -388,7 +388,7 @@ selected_classes = [0,1,2,3,4,5,6,7]
 X_train_init, y_train_init, X_test_init, y_test_init = load_synthetic_multiclass_linear(weights, input_dim=pca_dim, n_train=n_train_init, output_dim=n_qubits, n_test=400, noise_std=noise_std, seed=42)
 
 output_dim = y_train_init.shape[1]
-K = y_train_init.shape[1]  # K is output dimension too (not sure why I defined it twice...)
+K = y_train_init.shape[1]
 
 loss = nn.MSELoss() # at the moment it only works for MSE
 
